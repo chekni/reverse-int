848b1b373cc1d87d1ee0953bcdfceeb38d4b8ebd
chekni <karina_cheshko@mail.ru>
2021-02-05 14:07:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = n < 0 ? -1 * n : n;
+    let str = n.toString();
+    let countDigits = str.length;
+    let reverseN = "";
+    while (countDigits) {
+        reverseN += str[countDigits - 1];
+        countDigits--;
+    }
+    return +reverseN;
 }
